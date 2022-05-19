ef7df1fca334aa968660258df8197c86c3f60a02
the1323 <the132@gmail.com>
2022-05-18 21:57:08 -0700

    stencil code added

---
+++ test.js
@@ -0,0 +1,13 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.eus0n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
